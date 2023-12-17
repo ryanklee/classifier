26ed5a4c4b64d266b84bf607f440b066f95551cd
Ryan Kleeberger <rylklee@gmail.com>
2023-12-17 01:03:44 -0600

    Added script to rename and generate JSON file from markdown data.

--- rename_markdown.py
+++ rename_markdown.py
@@ -4,6 +4,11 @@ import argparse
 import re
 import json
 import json
+import json
+# Function to generate and create a JSON file
+def generate_json_file(data, filename='output.json'):
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 # Function to generate and create a JSON file
 def generate_json_file(data, filename='output.json'):
     with open(filename, 'w', encoding='utf-8') as f:
